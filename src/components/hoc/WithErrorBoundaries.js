f16884373310c12161f946a7122293c21d6e9bd8
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-04-26 18:14:51 +0300

    fix

--- src/components/hoc/WithErrorBoundaries.js
+++ src/components/hoc/WithErrorBoundaries.js
@@ -5,7 +5,7 @@ import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 export default () => {
   return WrappedComponent => {
     return class extends React.Component {
-      static displayName = `WithErrorBoundaries${WrappedComponent.name || 'Component'}`
+      static displayName = `WithErrorBoundaries(${WrappedComponent.name || 'Component'})`
       
       constructor(props, context) {
         super(props, context);
