cc298a6e80593a6c0894531a2ff1ca89b1a7b3d5
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-04-27 13:12:04 +0300

    added routing and created config file for that

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import router from '../../configs/router';
+
+const Navigation = () => {
+  return (
+    <nav>
+      {router.mainMenuLinks.map(link => (
+        <Link
+          key={link.path}
+          to={link.path}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
