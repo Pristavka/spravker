033c63c67c540b50ed67a2991689bc884f78f7c7
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2018-05-04 14:24:11 +0300

    added reselect to project and created memoize selectors fro organizations

--- src/components/OrganizationsList/OrganizationsList.js
+++ src/components/OrganizationsList/OrganizationsList.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// Actions from firebase
 import { fetchAndSubscribeInitialDataFromFirestore } from '../../actions/firebase/organizations';
+
+// Selectors for redux
+import { getOrganizationsFromStoreWithSelector } from '../../selectors/organizations';
 
 import WithErrorBoundaries from '../HOC/WithErrorBoundaries';
 import Organization from '../Organization/Organization';
 
 @WithErrorBoundaries()
 @connect(
-  ({ organizations: { organizations } }) => ({ orgs: organizations }),
+  store => getOrganizationsFromStoreWithSelector(store),
   { fetchAndSubscribeInitialDataFromFirestore }
 )
 export default class OrganizationsList extends React.Component {
